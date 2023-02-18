398e3e0c6bf01fc7d006a8f99ede06a4d2eea473
Aaravoss <morgan.nayet@protonmail.com>
2023-02-18 18:44:14 +0100

    Création GestionTournois

---
+++ app/src/main/GestionTournois.java
@@ -0,0 +1,30 @@
+/*
+ * @author Morgan Nayet             								18 Feb 2023
+ * Copyrights
+ */
+package main;
+
+/**
+ * Lance l'application
+ * @author Morgan Nayet
+ */
+public class GestionTournois {
+
+	/**
+	 * Lance l'application et
+	 * ferme l'application quand l'utilisateur le décide
+	 * @param args non utilisé
+	 */
+	public static void main(String[] args) {
+
+		lancerApplication();
+	}
+
+	/**
+	 * Lance l'ihm correspondant au menu d'accueil de l'application
+	 */
+	private static void lancerApplication() {
+		
+		//TODO lancer l'ihm
+	}
+}
