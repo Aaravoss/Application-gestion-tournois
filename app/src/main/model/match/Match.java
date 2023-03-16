83f1273afdb165949bc757e5046cda51cd2cfab5
Aaravoss <morgan.nayet@protonmail.com>
2023-03-16 16:55:48 +0100

    Stockage des equipes dans les matchs

--- app/src/main/model/match/Match.java
+++ app/src/main/model/match/Match.java
@@ -4,16 +4,22 @@
  */
 package main.model.match;
 
+import java.util.ArrayList;
+import java.util.List;
+import main.model.equipe.Equipe;
+
 /**
  * 
  * @author Morgan Nayet
  */
 public class Match {
-
+	
+	private List equipes;
+	
 	/**
 	 * Créer un nouveau match par défaut
 	 */
 	public Match() {
+		this.equipes = new ArrayList<Equipe>();
 	}
-
 }
