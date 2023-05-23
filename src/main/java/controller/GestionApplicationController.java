e0ef141df92b0e3abc1454dca93ba745102e2a6c
Aaravoss <morgan.nayet@protonmail.com>
2023-05-23 14:13:47 +0200

    envoi pour sauvegarde sur git

---
+++ src/main/java/controller/GestionApplicationController.java
@@ -0,0 +1,25 @@
+package controller;
+
+import java.io.PrintWriter;
+
+public class GestionApplicationController {
+
+    /**
+     * Charge les tournois enregistrés dans le fichier de sauvegarde
+     * ou créé ce fichier s'il est introuvable
+     */
+    public void initialisation(){
+
+        //extraireEtat()
+    }
+
+    public void fermerApplication(){
+
+        //sauvegarderEtat()
+    }
+
+    private void sauvegarderEtatApplication(){
+
+        PrintWriter writer
+    }
+}
