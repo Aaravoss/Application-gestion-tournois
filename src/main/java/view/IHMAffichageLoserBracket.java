05e032684bdbf070f768466431ef3878fe3ebae5
Eilinia <96683426+Eilinia@users.noreply.github.com>
2023-06-22 18:39:19 +0200

    Tentative affichage tournoi des perdants

--- src/main/java/view/IHMAffichageLoserBracket.java
+++ src/main/java/view/IHMAffichageLoserBracket.java
@@ -13,12 +13,15 @@ import javafx.scene.control.Alert;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
-import javafx.scene.layout.GridPane;
+import javafx.scene.layout.*;
+import javafx.scene.paint.Color;
 import javafx.scene.text.Font;
 import javafx.stage.Stage;
 import model.tour.Tour;
 import javafx.stage.WindowEvent;
 import model.tournoi.Tournoi;
+import model.tournoi.type.LoserBracket;
+
 import static utils.BusinessConstants.*;
 
 import java.util.ArrayList;
@@ -27,12 +30,14 @@ import java.util.List;
 public class IHMAffichageLoserBracket extends Application {
 
     private Tournoi tournoi;
-    private ArrayList<TextField> scores;
+    private ArrayList<TextField> scoresWB;
+    private ArrayList<TextField> scoresLB;
 
     public IHMAffichageLoserBracket(model.tournoi.Tournoi tournoi) {
 
         this.tournoi = tournoi;
-        this.scores = new ArrayList<>();
+        this.scoresWB = new ArrayList<>();
+        this.scoresLB = new ArrayList<>();
     }
 
     @Override
@@ -44,69 +49,95 @@ public class IHMAffichageLoserBracket extends Application {
         Label titre = new Label("Affichage tournoi Loser Bracket");
         titre.setFont(new Font("Cambria", TAILLE_TITRE));
         titre.setLayoutX(TAILLE_ECRAN_X /5);
-        titre.setLayoutY(50);
+        titre.setLayoutY(10);
 
 
-        int x = 50;
+        int x = 10;
+        int nbTour = 0;
         for(Tour tour : tournoi.getTours()) {
             GridPane listeMatch = new GridPane();
-            listeMatch.setHgap(3);
-            listeMatch.setVgap(10);
             listeMatch.setPadding(new Insets(50,50,50,50));
             listeMatch.setAlignment(Pos.CENTER);
             listeMatch.setLayoutX(x);
-            listeMatch.setLayoutY(100);
+            listeMatch.setLayoutY(60);
             List<model.match.Match> listeMatchs = tour.getMatchs();
             Label labelTour = new Label(tour.getNom());
             listeMatch.add(labelTour, 0,0 );
-            int i;
-            for (int j = 0; j < listeMatchs.size(); j++) {
+            for (int nbMatch = 0; nbMatch < listeMatchs.size(); nbMatch++) {
                 GridPane gridMatch = new GridPane();
                 gridMatch.setHgap(listeMatchs.size());
                 gridMatch.setVgap(2);
                 gridMatch.setPadding(new Insets(10,10,10,10));
-
-
-                i = 0;
-                for(model.equipe.Equipe equipe : listeMatchs.get(j).getEquipes() ) {
+                for(model.equipe.Equipe equipe : listeMatchs.get(nbMatch).getEquipes() ) {
                     if(tour == tournoi.getTourCourant()) {
+                        HBox hBoxMatch = new HBox();
+                        hBoxMatch.setBorder(new Border(new BorderStroke(Color.BLACK, BorderStrokeStyle.SOLID, CornerRadii.EMPTY, BorderWidths.DEFAULT)));
                         Label labelEquipe = new Label(equipe.getNom());
                         TextField textFieldScore = new TextField();
-                        double ligne;
-                        double colonne;
-                        if(gridMatch.getHgap() == 1){
-                            ligne = 1;
-                            colonne = i/gridMatch.getHgap();
-                        } else {
-                            ligne = i/gridMatch.getHgap();
-                            colonne = i%gridMatch.getHgap();
-                        }
-                        gridMatch.add(labelEquipe,(int) ligne, (int) colonne );
-                        gridMatch.add(textFieldScore, (int) ligne+1, (int) colonne);
-                        this.scores.add(textFieldScore);
-                        i++;
+                        hBoxMatch.getChildren().addAll(labelEquipe, textFieldScore);
+                        gridMatch.addColumn(nbTour,hBoxMatch);
+                        this.scoresWB.add(textFieldScore);
                     } else {
                         Label labelEquipe = new Label(equipe.getNom());
                         Label labelScore = new Label(""+ tour.getScore(equipe).getScore());
-                        double ligne = i/gridMatch.getHgap();
-                        double colonne = i%gridMatch.getHgap();
-                        gridMatch.add(labelEquipe,(int) ligne, (int) colonne );
-                        gridMatch.add(labelScore, (int) ligne+1, (int) colonne);
-                        i++;
+                        HBox hBoxMatch = new HBox();
+                        hBoxMatch.getChildren().addAll(labelEquipe, labelScore);
+                        gridMatch.addColumn(nbTour,hBoxMatch);
                     }
-
                 }
-
-                double ligne = j/gridMatch.getHgap();
-                double colonne = j%gridMatch.getHgap();
-                listeMatch.add(gridMatch, (int) ligne , (int) colonne +1);
-
-
+                listeMatch.addColumn(nbTour, gridMatch);
             }
+
             x += 300;
             root.getChildren().add(listeMatch);
+            nbTour++;
         }
 
+
+        if (nbTour >= 2 && tournoi instanceof LoserBracket) {
+            Tournoi tournoiLB = ((LoserBracket) tournoi).getLoserBracket();
+            double xLB = TAILLE_ECRAN_X - 300;
+            int nbTourLB = 0;
+            for(Tour tourLB : tournoiLB.getTours()) {
+                GridPane listeMatch = new GridPane();
+                listeMatch.setPadding(new Insets(50,50,50,50));
+                listeMatch.setAlignment(Pos.CENTER);
+                listeMatch.setLayoutX(xLB);
+                listeMatch.setLayoutY(60);
+                List<model.match.Match> listeMatchs = tourLB.getMatchs();
+                Label labelTour = new Label(tourLB.getNom());
+                listeMatch.add(labelTour, 0,0 );
+                for (int nbMatch = 0; nbMatch < listeMatchs.size(); nbMatch++) {
+                    GridPane gridMatch = new GridPane();
+                    gridMatch.setHgap(listeMatchs.size());
+                    gridMatch.setVgap(2);
+                    gridMatch.setPadding(new Insets(10,10,10,10));
+                    for(model.equipe.Equipe equipe : listeMatchs.get(nbMatch).getEquipes() ) {
+                        if(tourLB == tournoiLB.getTourCourant()) {
+                            HBox hBoxMatch = new HBox();
+                            hBoxMatch.setBorder(new Border(new BorderStroke(Color.BLACK, BorderStrokeStyle.SOLID, CornerRadii.EMPTY, BorderWidths.DEFAULT)));
+                            Label labelEquipe = new Label(equipe.getNom());
+                            TextField textFieldScore = new TextField();
+                            hBoxMatch.getChildren().addAll(labelEquipe, textFieldScore);
+                            gridMatch.addColumn(nbTourLB,hBoxMatch);
+                            this.scoresLB.add(textFieldScore);
+                        } else {
+                            Label labelEquipe = new Label(equipe.getNom());
+                            Label labelScore = new Label(""+ tourLB.getScore(equipe).getScore());
+                            HBox hBoxMatch = new HBox();
+                            hBoxMatch.getChildren().addAll(labelEquipe, labelScore);
+                            gridMatch.addColumn(nbTourLB,hBoxMatch);
+                        }
+                    }
+                    listeMatch.addColumn(nbTourLB, gridMatch);
+                }
+
+                xLB -= 300;
+                root.getChildren().add(listeMatch);
+                nbTourLB++;
+            }
+
+        }
 
         Button btnConfirmer = new Button();
         btnConfirmer.setLayoutX(TAILLE_ECRAN_X - TAILLE_BTN_X * 2 - TAILLE_ECRAN_X * 0.10);
@@ -118,16 +149,27 @@ public class IHMAffichageLoserBracket extends Application {
             @Override
             public void handle(ActionEvent actionEvent) {
 
-                int[] scoresEquipes;
+                int[] scoresEquipesWB;
+                int[] scoresEquipesLB;
 
-                scoresEquipes = new int[scores.size()];
+                scoresEquipesWB = new int[scoresWB.size()];
+                scoresEquipesLB = new int[scoresLB.size()];
 
-                for(int i = 0 ; i < scores.size() ; i++) {
+                for(int i = 0; i < scoresWB.size() ; i++) {
 
-                    scoresEquipes[i] = "".equals(scores.get(i).getText()) ? 0 : (int) Integer.parseInt(scores.get(i).getText());
+                    scoresEquipesWB[i] = "".equals(scoresWB.get(i).getText()) ? 0 : (int) Integer.parseInt(scoresWB.get(i).getText());
                 }
 
-                new GestionTournoiController().gererTournoi(stage, tournoi, scoresEquipes);
+                for(int i = 0; i < scoresLB.size() ; i++) {
+
+                    scoresEquipesLB[i] = "".equals(scoresLB.get(i).getText()) ? 0 : (int) Integer.parseInt(scoresLB.get(i).getText());
+                }
+
+                new GestionTournoiController().gererTournoi(stage, tournoi, scoresEquipesWB);
+                if(((LoserBracket)tournoi).getLoserBracket().getTourCourant() != null) {
+                    new GestionTournoiController().gererTournoi(stage, ((LoserBracket)tournoi).getLoserBracket(), scoresEquipesLB);
+                }
+
                 Alert alert = new Alert(Alert.AlertType.INFORMATION);
                 alert.setTitle("Confirmation");
                 alert.setHeaderText(null);
