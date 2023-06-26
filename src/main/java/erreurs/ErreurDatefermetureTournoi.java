5a98f22dce59589650202ac30d97faf72f09f926
Aaravoss <morgan.nayet@protonmail.com>
2023-06-26 12:14:02 +0200

    javadoc erreurs

--- src/main/java/erreurs/ErreurDatefermetureTournoi.java
+++ src/main/java/erreurs/ErreurDatefermetureTournoi.java
@@ -7,15 +7,18 @@ package erreurs;
 import java.util.Calendar;
 
 /**
- * 
+ * Exception pour la date de fermeture
+ *
  * @author Morgan Nayet
  */
 public class ErreurDatefermetureTournoi extends Error {
 
 	/**
-	 * 
-	 * TODO Javadoc - expliquer ce que fait le constructeur
-	 * @param dateFermetureInvalide
+	 * Exception lancée si la date de fermeture est supérieure à la date d'ouverture
+	 *
+	 * @param dateFermetureInvalide date de fermeture invalide affetée à un tournoi
+	 *
+	 * @author Morgan Nayet
 	 */
 	public ErreurDatefermetureTournoi(Calendar dateFermetureInvalide) {
 		
